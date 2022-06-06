51d8c0a3e5d1b74dc3819e5f8f8a77dc9bcf443a
Aydeke <aydeke2@gmail.com>
2022-06-06 21:16:11 +0600

    Add bigvideo

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, res) => {
-  fs.readFile("./bigfiles/bigfile.txt", (err, data) => {
-    if (err) throw err;
+  // fs.readFile("./bigfiles/bigfile.txt", (err, data) => {
+  //   if (err) throw err;
 
-    res.end(data);
-  });
+  //   res.end(data);
+  // });
+
+  const bigfile = fs.createReadStream("./bigfiles/bigfile.txt");
+  const bigvideo = fs.createReadStream("./bigfiles/bigvideo.mp4");
+  res.setHeader("Content-Type", "video/mp4");
+  bigvideo.pipe(res);
 });
 
 const PORT = process.env.PORT || 3000;
